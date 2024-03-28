d03f820f869b071382d85464710feaf538743488
Matias Pencef <matiaspencef@gmail.com>
2024-03-28 13:46:42 +0100

    Añadí atributos y constructores.

--- src/starWars/Jedi.java
+++ src/starWars/Jedi.java
@@ -1,5 +1,21 @@
 package starWars;
 
+import java.util.Random;
+
 public class Jedi {
+
+	private String nombre;
+	private int salud;
+	private int fuerza;
+	private Random random;
+	private String tipoCapa;
+
+	public Jedi(String nombre, int salud, int fuerza, String tipoCapa) {
+		super();
+		this.nombre = nombre;
+		this.salud = salud;
+		this.fuerza = fuerza;
+		this.tipoCapa = tipoCapa;
+	}
 
 }
--- src/starWars/Sith.java
+++ src/starWars/Sith.java
@@ -1,5 +1,21 @@
 package starWars;
 
+import java.util.Random;
+
 public class Sith {
+
+	private String nombre;
+	private int salud;
+	private int fuerza;
+	private Random random;
+	private String tipoEspada;
+
+	public Sith(String nombre, int salud, int fuerza, String tipoEspada) {
+		super();
+		this.nombre = nombre;
+		this.salud = salud;
+		this.fuerza = fuerza;
+		this.tipoEspada = tipoEspada;
+	}
 
 }
