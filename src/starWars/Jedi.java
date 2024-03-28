2fa538261687eaa08ced82532decd012898c3af8
Matias Pencef <matiaspencef@gmail.com>
2024-03-28 15:10:55 +0100

    Creadas todas las funciones.

--- src/starWars/Jedi.java
+++ src/starWars/Jedi.java
@@ -16,6 +16,66 @@ public class Jedi {
 		this.salud = salud;
 		this.fuerza = fuerza;
 		this.tipoCapa = tipoCapa;
+		this.random = new Random();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getSalud() {
+		return salud;
+	}
+
+	public void setSalud(int salud) {
+		this.salud = salud;
+	}
+
+	public int getFuerza() {
+		return fuerza;
+	}
+
+	public void setFuerza(int fuerza) {
+		this.fuerza = fuerza;
+	}
+
+	public String getTipoCapa() {
+		return tipoCapa;
+	}
+
+	public void setTipoCapa(String tipoCapa) {
+		this.tipoCapa = tipoCapa;
+	}
+
+	@Override
+	public String toString() {
+		return "Jedi [nombre=" + nombre + ", salud=" + salud + ", fuerza=" + fuerza + ", tipoCapa=" + tipoCapa + "]";
+	}
+
+	public void imprimir() {
+		System.out.println("Nombre: " + this.nombre);
+		System.out.println("Salud: " + this.salud);
+		System.out.println("Fuerza: " + this.fuerza);
+		System.out.println("Tipo de capa: " + this.tipoCapa);
+	}
+
+	public void atacar(Sith sith) {
+		int danyo = random.nextInt(30) + 30; //Crea numero aleatorio entre 30 y 60.
+		System.out.println(this.nombre + " ataca a " + sith.getNombre() + "!");
+		sith.recibirAtaque(danyo);
+	}
+
+	public void recibirAtaque(int danyo) {
+		this.salud -= danyo;
+		if (this.salud <= 0) {
+			System.out.println(this.nombre + " ha sido derrotado.");
+		} else {
+			System.out.println(this.nombre + " recibe " + danyo + " puntos de daño. Salud restante: " + this.salud);
+		}
 	}
 
 }
--- src/starWars/Sith.java
+++ src/starWars/Sith.java
@@ -16,6 +16,67 @@ public class Sith {
 		this.salud = salud;
 		this.fuerza = fuerza;
 		this.tipoEspada = tipoEspada;
+		this.random = new Random();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getSalud() {
+		return salud;
+	}
+
+	public void setSalud(int salud) {
+		this.salud = salud;
+	}
+
+	public int getFuerza() {
+		return fuerza;
+	}
+
+	public void setFuerza(int fuerza) {
+		this.fuerza = fuerza;
+	}
+
+	public String getTipoEspada() {
+		return tipoEspada;
+	}
+
+	public void setTipoEspada(String tipoEspada) {
+		this.tipoEspada = tipoEspada;
+	}
+
+	@Override
+	public String toString() {
+		return "Sith [nombre=" + nombre + ", salud=" + salud + ", fuerza=" + fuerza + ", tipoEspada=" + tipoEspada
+				+ "]";
+	}
+
+	public void imprimir() {
+		System.out.println("Nombre: " + this.nombre);
+		System.out.println("Salud: " + this.salud);
+		System.out.println("Fuerza: " + this.fuerza);
+		System.out.println("Tipo de capa: " + this.tipoEspada);
+	}
+
+	public void atacar(Jedi jedi) {
+		int danyo = random.nextInt(30) + 30; // Crea numero aleatorio entre 30 y 60.
+		System.out.println(this.nombre + " ataca a " + jedi.getNombre() + "!");
+		jedi.recibirAtaque(danyo);
+	}
+
+	public void recibirAtaque(int dano) {
+		this.salud -= dano;
+		if (this.salud <= 0) {
+			System.out.println(this.nombre + " ha sido derrotado.");
+		} else {
+			System.out.println(this.nombre + " recibe " + dano + " puntos de daño. Salud restante: " + this.salud);
+		}
 	}
 
 }
